caa0199f960e308c9c6414bf7499868d5b050998
Carl Pacey <Carl.Pacey@D2L.com>
2020-03-27 15:07:22 -0400

    Initial commit with stupid simple converter and tests

---
+++ src/Converter/Transformer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Converter {
+	public static class Transformer {
+
+		public static IEnumerable<string> TransformLines(
+			IEnumerable<string> inputLines
+		) {
+			return inputLines.Select( TransformLine );
+		}
+
+		private static string TransformLine( string inputLine ) {
+			(string name, int count) = ReadLineData( inputLine );
+
+			return $"{name} has {count} items";
+		}
+
+		private static (string name, int count) ReadLineData( string inputLine ) {
+			string[] items = inputLine.Split( ',' );
+
+			if( items.Length != 2 ) {
+				throw new ArgumentException( $"Line contains unexpected number of items: \"{inputLine}\"" );
+			}
+
+			string name = items[0];
+			int count = int.Parse( items[1] );
+
+			return ( name, count );
+		}
+	}
+}
---
+++ src/ConverterTests/TransformerTests.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using Converter;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+
+namespace ConverterTests {
+	public class Tests {
+
+		public static IEnumerable<ITestCaseData> TransformLines_TestCases() {
+			yield return
+				new TestCaseData(
+					arg: new[] {"carl,19"}
+				).Returns(
+					new[] {"carl has 19 items"}
+				);
+		}
+
+		[TestCaseSource(nameof(TransformLines_TestCases))]
+		public IEnumerable<string> TransformLines( string[] inputLine ) {
+			IEnumerable<string> result = Transformer.TransformLines( inputLine );
+			return result;
+		}
+	}
+}
